c2c8fd1df2733b7f995aabb3af179680dcade997
Nir Maoz <maoznir@gmail.com>
2018-01-23 16:27:26 +0200

    added repository to package.json

--- letsdothis.js
+++ letsdothis.js
@@ -1,20 +1,20 @@
 module.exports = (input, promises) => {
-        if (!promises || !Array.isArray(promises) || !promises.length){
-            return new Promise((resolve, reject) => {
-                try{
-                    return resolve(input);
-                }
-                catch(e){
-                    return reject(e);
-                }
-            });
-        }
+    if (!promises || !Array.isArray(promises) || !promises.length) {
+        return new Promise((resolve, reject) => {
+            try {
+                return resolve(input);
+            }
+            catch (e) {
+                return reject(e);
+            }
+        });
+    }
 
-        let promise = promises[0](input);
+    let promise = promises[0](input);
 
-        for (let i = 1; i < promises.length; i++){
-            promise = promise.then(promises[i]);
-        }
+    for (let i = 1; i < promises.length; i++) {
+        promise = promise.then(promises[i]);
+    }
 
-        return promise;
+    return promise;
 };
